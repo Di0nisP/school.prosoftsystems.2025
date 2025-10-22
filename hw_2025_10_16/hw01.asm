453bb126c07fe9e17105d7cdaa99a2ec79c5cd80
Di0nisP <ya.amico@yandex.ru>
2025-10-22 09:05:37 +0500

    update(hw01)

--- hw_2025_10_16/hw01.asm
+++ hw_2025_10_16/hw01.asm
@@ -1,7 +1,7 @@
 global _start                      ; делаем метку метку _start видимой извне
  
 section .data                      ; секция данных
-    message db  "Hello world!",10  ; строка для вывода на консоль
+    message db  "Hello, world",10  ; строка для вывода на консоль
     length  equ $ - message
  
 section .text                      ; объявление секции кода
@@ -13,15 +13,15 @@ _start:                            ; точка входа в программу
     syscall                        ; выполняем системный вызов write
  
     ; Проверка результата (результат syscall -> rax)
-    cmp rax, 0                     ; `rax - 0` - установка флагов
-    jl error                       ; выполняет переход, если rax < 0
+    cmp rax, length                ; `rax - length` - установка флагов
+    jl _error                      ; выполняет переход, если rax < length (записаны не все байты)
 
     ; Успех -> exit(0)
     mov rax, 60                    ; 60 - номер системного вызова exit
     mov rdi, 0                     ; успешное завершение
     syscall                        ; выполняем системный вызов exit
 
-error:
+_error:
     mov rax, 60                    ; 60 - номер системного вызова exit
     mov rdi, 1                     ; код ошибки - 1
     syscall                        ; выполняем системный вызов exit
